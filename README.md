8cddba5d08e6ede7f363c892ed02863f270e08d2
Well404 <40534114+Well2333@users.noreply.github.com>
2025-01-24 12:22:38 +0800

    ✨ 引入缓存机制以优化动态获取性能 (#21)

--- README.md
+++ README.md
@@ -2,10 +2,7 @@
 
 > api docs: https://apifox.com/apidoc/shared-4c1ba1cb-aa98-4a24-9986-193ab8f1519e/246937366e0
 
-> config: https://github.com/Well2333/bilichat-request/blob/main/src/bilichat_request/config.py
-
 > cookiecloud: https://github.com/easychen/CookieCloud/blob/master/README_cn.md
->
 
 ## 安装并运行
 
@@ -21,3 +18,16 @@ pipx install bilichat-request
 ```shell
 bilirq
 ```
+
+## 调整配置
+
+在工作路径下创建 `config.yaml` 文件，并向其中添加所需要调整的内容即可，例如：
+
+```yaml
+cookie_clouds:
+  - url: https://example.com
+    uuid: ********
+    password: ********
+```
+
+具体的配置项及默认值可以参考 [config.py](https://github.com/Well2333/bilichat-request/blob/main/src/bilichat_request/config.py)。
--- src/bilichat_request/functions/subs/dynamic/__init__.py
+++ src/bilichat_request/functions/subs/dynamic/__init__.py
@@ -2,14 +2,33 @@ from collections.abc import Callable
 from functools import wraps
 from json import JSONDecodeError
 
+from cachetools import TTLCache
 from httpx import ConnectError, TransportError
 from loguru import logger
 from sentry_sdk import capture_exception
 
 from bilichat_request.account import get_web_account
+from bilichat_request.config import config
 from bilichat_request.exceptions import AbortError, ResponseCodeError
 
 from .model import CARD_TYPE_MAP, Dynamic, DynamicType
+
+dyn_cache = TTLCache(maxsize=99999, ttl=config.dynamic_cache_ttl or 1)  # type: ignore
+
+
+def cache_dynamics(func: Callable) -> Callable:
+    @wraps(func)
+    async def wrapper(up_uid: int, *args, **kwargs) -> list[Dynamic]:
+        if not kwargs.get("offset", 0) and config.dynamic_cache_ttl and (cached := dyn_cache.get(up_uid)):
+            logger.debug(f"使用缓存: {up_uid}")
+            return cached
+
+        result = await func(up_uid, *args, **kwargs)
+        if config.dynamic_cache_ttl:
+            dyn_cache[up_uid] = result
+        return result
+
+    return wrapper
 
 
 def dyn_error_handler(func: Callable):
@@ -35,13 +54,14 @@ def dyn_error_handler(func: Callable):
 
 
 @dyn_error_handler
+@cache_dynamics
 async def get_dynamic_by_uid(up_uid: int, offset: int = 0) -> list[Dynamic]:
-    ids: list[Dynamic] = []
+    dyns: list[Dynamic] = []
     async with get_web_account() as account:
         resp = await account.web_requester.get_user_dynamics(up_uid, offset=offset)
         items = resp.get("items", [])
         if items:
-            ids.extend(
+            dyns.extend(
                 [
                     Dynamic(
                         dyn_id=int(item["id_str"]),
@@ -51,17 +71,18 @@ async def get_dynamic_by_uid(up_uid: int, offset: int = 0) -> list[Dynamic]:
                     for item in items
                 ]
             )
-    return ids
+    return dyns
 
 
 @dyn_error_handler
+@cache_dynamics
 async def get_dynamic_by_uid_old(up_uid: int) -> list[Dynamic]:
-    ids: list[Dynamic] = []
+    dyns: list[Dynamic] = []
     async with get_web_account() as account:
         resp = await account.web_requester.get_user_dynamics_old(up_uid)
         cards = resp.get("cards", [])
         if cards:
-            ids.extend(
+            dyns.extend(
                 [
                     Dynamic(
                         dyn_id=int(card["desc"]["dynamic_id_str"]),
@@ -71,24 +92,4 @@ async def get_dynamic_by_uid_old(up_uid: int) -> list[Dynamic]:
                     for card in cards
                 ]
             )
-    return ids
-
-
-@dyn_error_handler
-async def get_dynamic_by_acc_sub(acc_uid: int, offset: int) -> list[Dynamic]:
-    ids: list[Dynamic] = []
-    async with get_web_account(account_uid=acc_uid) as account:
-        resp = await account.web_requester.get_all_dynamics_list(offset=offset)
-        items = resp.get("items", [])
-        if items:
-            ids.extend(
-                [
-                    Dynamic(
-                        dyn_id=int(item["id_str"]),
-                        dyn_type=item["type"],
-                        dyn_timestamp=item["modules"]["module_author"]["pub_ts"],
-                    )
-                    for item in items
-                ]
-            )
-    return ids
+    return dyns
